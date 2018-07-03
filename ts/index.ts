4e70a1ef1a021f0c863cef332bb6936fbe38b712
Phil Kunz <gitlab@philkunz.com>
2018-07-03 14:44:57 +0200

    fix(CI): update default ci config

--- ts/index.ts
+++ ts/index.ts
@@ -3,12 +3,12 @@ import * as tsNode from 'ts-node';
 
 const defaultTsNodeOptions: tsNode.Options = {
   compilerOptions: {
-    lib: ['es2016','es2017'],
-    target: 'es2015',
+    lib: ['es2016', 'es2017'],
+    target: 'es2015'
   },
   skipIgnore: true,
   cacheDirectory: path.join(__dirname, '../tscache')
-}
+};
 
 if (process.argv.includes('--nocache')) {
   defaultTsNodeOptions.cache = false;
@@ -20,8 +20,8 @@ if (process.env.CLI_CALL_TSRUN) {
   // contents of argv array
   // process.argv[0] -> node Executable
   // process.argv[1] -> tsrun executable
-  const pathToTsFile = process.argv[2]
-  
+  const pathToTsFile = process.argv[2];
+
   const pathToLoad = path.join(process.cwd(), pathToTsFile);
   import(pathToLoad);
 }

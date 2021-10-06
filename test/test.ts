282d8b2b9398f38ecc506c583bb494a769a8c68d
Philipp Kunz <gitlab@philkunz.com>
2021-10-06 13:06:24 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -1,2 +1,10 @@
 const textToPost: string = 'Test runs!';
 console.log(textToPost);
+
+const run = async () => {
+  // lets test esm
+  console.warn('remember to enable esm checks once TypeScript 4.5 is released.')
+  // const nodeFetch = await import('node-fetch');
+}
+
+run();
--- ts/index.ts
+++ ts/index.ts
@@ -5,10 +5,12 @@ import { CompilerOptions } from 'typescript';
 const defaultTsNodeOptions: tsNode.CreateOptions = {
   compilerOptions: {
     lib: ['es2016', 'es2017', 'dom'],
-    target: <any>'es2017', // Script Target should be a string -> 2 is for ES2015
+    target: <any>'es2020', // Script Target should be a string -> 2 is for ES2015
     experimentalDecorators: true,
     esModuleInterop: true,
     strictNullChecks: false,
+    moduleResolution: <any>'node',
+    importsNotUsedAsValues: <any>'preserve',
   } as CompilerOptions,
   skipIgnore: true,
 };

a240cdfde18451bce46c5bd1c2cb961a02a69b54
Philipp Kunz <gitlab@philkunz.com>
2022-05-25 10:11:14 +0200

    fix(core): update

---
+++ ts/00_commitinfo_data.ts
@@ -0,0 +1,8 @@
+/**
+ * autocreated commitinfo by @pushrocks/commitinfo
+ */
+export const commitinfo = {
+  name: '@gitzone/tsrun',
+  version: '1.2.33',
+  description: 'run typescript programs efficiently'
+}
